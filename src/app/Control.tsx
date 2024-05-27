f3534f2a1eb3c606fbaf86f49ac7d5edf6c3101e
mood <mooddddd@naver.com>
2024-05-27 23:38:18 +0900

    delete 추가

--- src/app/Control.tsx
+++ src/app/Control.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 export const Control = () => {
   const params = useParams(); // 이렇게 따로 컴포넌트 빼서 훅 사용하면 됨!!
+  const router = useRouter();
   const id = params.id;
   return (
     <ul>
@@ -16,7 +17,19 @@ export const Control = () => {
             <a href={`/update/` + id}>Update</a>
           </li>
           <li>
-            <input type='button' value='delete' />
+            <input
+              type='button'
+              value='delete'
+              onClick={() => {
+                const options = { method: 'DELETE' };
+                fetch('http://localhost:9999/topics/' + id, options)
+                  .then((res) => res.json())
+                  .then((result) => {
+                    router.push('/');
+                    router.refresh();
+                  });
+              }}
+            />
           </li>
         </>
       ) : null}
